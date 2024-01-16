c90163f464370fda889d51ed2b099e87b31309a2
Obama <obama0799@gmail.com>
2024-01-16 22:10:31 +0800

    <更新>一键启动文件换行符

--- server_v2/config/app.php
+++ server_v2/config/app.php
@@ -15,7 +15,7 @@
 use support\Request;
 
 return [
-    'debug' => false,
+    'debug' => true,
     'error_reporting' => E_ALL,
     'default_timezone' => 'Asia/Shanghai',
     'request_class' => Request::class,
